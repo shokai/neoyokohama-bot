6caa13f84aa628e978ff92204fc43acb5f5ef6c1
Sho Hashimoto <hashimoto@shokai.org>
2015-12-19 18:03:24 +0900

    bugfix util.isScheduleToday()

--- libs/util.js
+++ libs/util.js
@@ -4,7 +4,7 @@ export default {
   isScheduleToday: function(schedule){
     const today = new Date();
     return today.getFullYear() === schedule.year &&
-      today.getMonth() === schedule.month &&
+      today.getMonth()+1 === schedule.month &&
       today.getDate() === schedule.date;
   }
 }

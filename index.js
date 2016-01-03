72ea91f7c8f7379841f2faaee297f9e53bfc35b7
Sho Hashimoto <hashimoto@shokai.org>
2016-01-04 01:53:53 +0900

    separate runner for local machine into "index.js"

--- index.js
+++ index.js
@@ -3,9 +3,4 @@
 if(!process.env.DEBUG) process.env.DEBUG = "bot*";
 process.env.TZ = "Asia/Tokyo";
 
-var bot = require("./dist/main");
-module.exports.handler = bot.handler;
-
-if(process.argv[1] === __filename){
-  bot.handler();
-}
+module.exports.handler = require("./dist/main").handler;
---
+++ run.js
@@ -0,0 +1,4 @@
+"use strict";
+
+var index = require("./index");
+index.handler();

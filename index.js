0971ad894aac2da426069bede380954dc420ae79
Sho Hashimoto <hashimoto@shokai.org>
2016-01-19 04:02:57 +0900

    modify DEBUG to show weather-yahoo-jp's message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-if(!process.env.DEBUG) process.env.DEBUG = "bot*";
+if(!process.env.DEBUG) process.env.DEBUG = "bot*,weather*";
 process.env.TZ = "Asia/Tokyo";
 
 module.exports.handler = require("./dist/main").handler;
